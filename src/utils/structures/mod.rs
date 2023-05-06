6d592f85142bc5a7273c86dc96e716070ee43079
Patrick <patrykrzucidlo1@gmail.com>
2023-05-06 13:59:57 +0200

    feat: add more accessors and broad up the type of predicate argument in tree structure

--- src/utils/structures/mod.rs
+++ src/utils/structures/mod.rs
@@ -1,2 +1,1 @@
-pub mod json_tree;
 pub mod tree;
--- src/utils/structures/tree.rs
+++ src/utils/structures/tree.rs
@@ -1,7 +1,9 @@
+use std::rc::Rc;
+
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, PartialEq, Deserialize, Serialize)]
-struct Node<T> {
+pub struct Node<T> {
     data: T,
     children: Vec<Node<T>>,
 }
@@ -17,6 +19,10 @@ impl<T> Node<T> {
             data,
             children: Vec::new(),
         }
+    }
+
+    pub fn data(&self) -> &T {
+        &self.data
     }
 
     pub fn add_child(&mut self, data: T) -> &mut Node<T> {
@@ -37,20 +43,40 @@ impl<T> Tree<T> {
         }
     }
 
+    pub fn root(&self) -> &Node<T> {
+        &self.root
+    }
+
     pub fn root_mut(&mut self) -> &mut Node<T> {
         &mut self.root
     }
 
-    pub fn find_mut(&mut self, predicate: fn(&T) -> bool) -> Option<&mut Node<T>> {
-        Tree::traverse(&mut self.root, predicate)
+    pub fn find_mut<F>(&mut self, predicate: F) -> Option<&mut Node<T>>
+    where
+        F: Fn(&T) -> bool,
+    {
+        Self::traverse_mut(&mut self.root, Rc::new(predicate))
     }
 
-    fn traverse(node: &mut Node<T>, predicate: fn(&T) -> bool) -> Option<&mut Node<T>> {
+    pub fn find<F>(&mut self, predicate: F) -> Option<&Node<T>>
+    where
+        F: Fn(&T) -> bool,
+    {
+        match Self::traverse_mut(&mut self.root, Rc::new(predicate)) {
+            Some(node) => Some(&*node),
+            None => None,
+        }
+    }
+
+    fn traverse_mut<F>(node: &mut Node<T>, predicate: Rc<F>) -> Option<&mut Node<T>>
+    where
+        F: Fn(&T) -> bool,
+    {
         for child in &mut node.children {
             if predicate(&child.data) {
                 return Some(child);
             } else if child.has_children() {
-                match Tree::traverse(child, predicate) {
+                match Self::traverse_mut(child, predicate.clone()) {
                     Some(child) => return Some(child),
                     None => continue,
                 }
@@ -70,6 +96,8 @@ mod tests {
 
         assert_eq!(tree.find_mut(|d| d == &5).unwrap().data, 5);
         assert_eq!(tree.find_mut(|d| d == &999), None);
+        assert_eq!(tree.find(|d| d == &5).unwrap().data, 5);
+        assert_eq!(tree.find(|d| d == &999), None);
     }
 
     fn build_tree() -> Tree<i32> {
